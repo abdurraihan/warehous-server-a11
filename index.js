180c6c90c2d360ce15fa10523bb2c65946d86778
abdurraihan <abdurraihan898@gmail.com>
2022-05-07 16:21:24 +0600

    after make get api

--- index.js
+++ index.js
@@ -19,27 +19,50 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://DB_USER:DB_PASS@cluster0.3j664.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3j664.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log('mongodb running')
-  client.close();
-});
+
+async function run() {
+
+
+    try {
+
+        await client.connect();
+        const productsCollection = client.db('warehous').collection('products');
+
+
+        //get api for all products
+        app.get('/products', async (req, res) => {
+
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+
+            res.send(products);
+
+
+        })
+
+
+    } finally {
+
+    }
+}
+
+
+run().catch(console.dir);
 
 
 
 
-
-app.get('/' , (req,res)=>{
+app.get('/', (req, res) => {
     res.send('running warehous-server ');
 
 })
 
 
 
-app.listen(port ,()=>{
-    console.log('listening to port' , port);
-} )
+app.listen(port, () => {
+    console.log('listening to port', port);
+})
